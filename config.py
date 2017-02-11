6ba7569ff48b7b90556baac0b1d2fc791d98a3bb
TheSwagMan <theswagman@gmx.fr>
2017-02-11 01:41:13 +0100

    Moving code

---
+++ config.py
@@ -0,0 +1,9 @@
+SERVER_HOSTNAME="musicswagger_server"
+# hostname du server
+SERVER_PORT=55666
+# port du server
+DEBUG_MODE=True
+# mode debug
+def log(*args):
+    if DEBUG_MODE:
+        print(*args)
